d40b0f6008440d06b9f635a6926ea456a51b1b02
Matyáš Boreček <matyas.borecek@unicorn.com>
2022-01-16 19:51:40 +0100

    Add header

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import Header from "../Header/Header";
+
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    )
+        ;
+}
+
+export default App;
